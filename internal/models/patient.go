6ed9657ee284facf00d13d2e054f81551e6546bb
Patorn Kwanthale <patorn.k@extend-it-resource.com>
2026-01-12 22:05:11 +0700

    fix: gorm struct tag

--- internal/models/patient.go
+++ internal/models/patient.go
@@ -6,12 +6,12 @@ type Patient struct {
 	ID           int       `gorm:"primaryKey" json:"id"`
 	FirstNameTh  string    `gorm:"index:,type:btree" json:"first_name_th" validate:"required,min=1,max=100"`
 	MiddleNameTh string    `json:"middle_name_th" validate:"max=100"`
-	LastNameTh   string    `gorm:"index:,type:btree" json:"last_name_th" validate:"required,min=1,max=100"`
+	LastNameTh   string    `gorm:"index:" json:"last_name_th" validate:"required,min=1,max=100"`
 	FirstNameEn  string    `json:"first_name_en" validate:"required,min=1,max=100"`
 	MiddleNameEn string    `json:"middle_name_en" validate:"max=100"`
 	LastNameEn   string    `json:"last_name_en" validate:"required,min=1,max=100"`
 	DateOfBirth  time.Time `json:"date_of_birth" validate:"required"`
-	PatientHN    string    `gorm:"uniqueIndex" json:"patient_hn" validate:"required,min=1,max=50"`
+	PatientHN    string    `gorm:"index" json:"patient_hn" validate:"required,min=1,max=50"`
 	NationalID   string    `gorm:"uniqueIndex" json:"national_id" validate:"required,min=1,max=20"`
 	PassportID   string    `gorm:"uniqueIndex" json:"passport_id" validate:"max=50"`
 	PhoneNumber  string    `json:"phone_number" validate:"required,min=1,max=20"`
